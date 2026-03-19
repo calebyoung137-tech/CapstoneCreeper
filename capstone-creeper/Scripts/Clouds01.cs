c6c6ce6cfc7bfb4ff0506a1e65b9f3c47056d0ce
Caleb Young <calebyoung137@gmail.com>
2026-03-19 18:01:16 -0500

    More changes

---
+++ capstone-creeper/Scripts/Clouds01.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Clouds01 : Sprite2D
+{
+	[Export] public float Speed = 50f;
+
+	public override void _Process(double delta)
+	{
+		// Move to the right
+		Position += new Vector2(Speed * (float)delta, 0);
+
+		// If it goes off screen, reset to left
+		if (Position.X > GetViewportRect().Size.X + 100) // 100 = buffer
+		{
+			Position = new Vector2(-100, Position.Y);
+		}
+	}
+}
