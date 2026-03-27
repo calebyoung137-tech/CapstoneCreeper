fd5d055cd9ffdc4ab9d55bb8f79004b676426d9e
Caleb Young <calebyoung137@gmail.com>
2026-03-26 21:50:05 -0700

    adding dialog box starting

---
+++ capstone-creeper/Scripts/buttons/Exit.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class Exit : TextureButton
+{
+	private ConfirmationDialog _dialog;
+
+	public override void _Ready()
+	{
+		// Create the dialog
+		_dialog = new ConfirmationDialog();
+		_dialog.Title = "Are you sure?";
+		_dialog.DialogText = "Do you want to continue?";
+		_dialog.Hide();
+		AddChild(_dialog); // IMPORTANT: must be in the scene tree
+		_dialog.AddThemeFontSizeOverride("font_size", 32);
+		_dialog.Size = new Vector2I(600, 300); // width, height
+		Pressed += OnButtonPressed;
+	}
+
+	private void OnButtonPressed()
+	{
+		_dialog.PopupCentered();
+	}
+}
