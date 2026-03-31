46b60b8b95d003e16f30b37cec104cf5f4b50c7e
ksaelzler <ksaelzler@harding.edu>
2026-03-31 03:24:02 -0500

    Restoring previous changes

--- capstone-creeper/Scripts/View/MainMenu.cs
+++ capstone-creeper/Scripts/View/MainMenu.cs
@@ -2,52 +2,41 @@ using Godot;
 
 public partial class MainMenu : Control
 {
+	private Button _playButton;
+	private AudioStreamPlayer _clickSound;
 
-    
-    private Button _playButton;
-    //private Button _helpButton;
+	public override void _Ready()
+	{
+		_playButton = GetNode<Button>("VBoxContainer/PlayButton");
+		_clickSound = GetNode<AudioStreamPlayer>("VBoxContainer/Click");
 
-    public override void _Ready()
-    {
-        _playButton = GetNode<Button>("VBoxContainer/PlayButton");
-        //_helpButton = GetNode<Button>("VBoxContainer/HelpButton");
+		_playButton.Text = "Start";
+		_playButton.Pressed += OnPlayPressed;
 
-        _playButton.Text = "Start";
-        //_helpButton.Text = "Help";
+		// Button styling
+		var style = new StyleBoxFlat();
+		style.BgColor = new Color(0, 0, 0, 0); // fully transparent
+		style.BorderWidthLeft = 0;
+		style.BorderWidthRight = 0;
+		style.BorderWidthTop = 0;
+		style.BorderWidthBottom = 0;
 
-        _playButton.Pressed += OnPlayPressed;
-        //_helpButton.Pressed += OnHelpPressed;
+		_playButton.AddThemeStyleboxOverride("normal", style);
+		_playButton.AddThemeStyleboxOverride("hover", style);
+		_playButton.AddThemeStyleboxOverride("pressed", style);
+		_playButton.AddThemeStyleboxOverride("focus", style);
 
-        var style = new StyleBoxFlat();
-        style.BgColor = new Color(0, 0, 0, 0); // fully transparent
-        style.BorderWidthLeft = 0;
-        style.BorderWidthRight = 0;
-        style.BorderWidthTop = 0;
-        style.BorderWidthBottom = 0;
+		_playButton.AddThemeColorOverride("font_color", new Color(1, 1, 1)); // white
+		_playButton.AddThemeColorOverride("font_hover_color", new Color(1, 1, 0)); // yellow
+		_playButton.AddThemeFontSizeOverride("font_size", 122);
+	}
 
-        _playButton.AddThemeStyleboxOverride("normal", style);
-        _playButton.AddThemeStyleboxOverride("hover", style);
-        _playButton.AddThemeStyleboxOverride("pressed", style);
-        _playButton.AddThemeStyleboxOverride("focus", style);
+	private void OnPlayPressed()
+	{
+		// Play click sound
+		_clickSound?.Play();
 
-        _playButton.AddThemeColorOverride("font_color", new Color(1, 1, 1)); // white
-
-        // Hover text color
-
-        _playButton.AddThemeColorOverride("font_hover_color", new Color(1, 1, 0)); // yellow
-
-        _playButton.AddThemeFontSizeOverride("font_size", 122);
-
-    }
-
-    private void OnPlayPressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/play_menu.tscn");
-    }
-
-    /*private void OnHelpPressed()
-    {
-        GD.Print("Help pressed");
-        // Later: show help popup or load help scene
-    }*/
+		// Change scene
+		GetTree().ChangeSceneToFile("res://scenes/play_menu.tscn");
+	}
 }
