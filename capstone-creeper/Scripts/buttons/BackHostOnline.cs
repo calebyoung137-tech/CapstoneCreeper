d1ffcc06ab3626ed0b006fd242a7451326087e4b
Your Name <youremail@example.com>
2026-04-03 16:50:05 -0500

    Major menu tweaks

---
+++ capstone-creeper/Scripts/buttons/BackHostOnline.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class BackHostOnline : TextureButton
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += goToOnline; 
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void goToOnline()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/online_multiplayer_menu.tscn");
+	}
+   }
